14070916c64afffc21def9fc20768fcfba9eb0de
Egor <tohtuev.egoor@yandex.ru>
2024-03-16 13:49:13 +0500

    add main page and nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<nav>
+    <div class="nav-container">
+        <a href="/" class="nav-logo" title="Back to Homepage">Егор Тохтуев</a>
+        <div class="nav-links">
+            <a href="/about" class="link">Студент СГХТ</a>
+            <a href="/blog" class="link">Живу в России</a>
+            <a href="/projects" class="link">Портфолио</a>
+            <a href="/contact" class="link">Незнаю</a>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <!-- Page will be injected below -->
+    <slot></slot>
+</div>
+
+<style>
+    .container {
+        max-width: 1400px;
+        margin: 50px auto;
+    }
+</style>
