45a50c3657a71c0e6124378c11ee741797b95156
Egor <tohtuev.egoor@yandex.ru>
2024-03-17 12:28:17 +0500

    temp

--- src/routes/temp/+page.svelte
+++ src/routes/temp/+page.svelte
@@ -1,3 +1,34 @@
-<h1>temp</h1>
+<script>
+    let name = "Svelte"
+    let counter = 0;
 
-todo...
+    $: counterClass = counter % 2 === 0 ? 'red' : 'blue';
+
+    $: upperName = name.toUpperCase();
+    $: lowerName = name.toLowerCase();
+
+    function chaneName() {
+        name = "New name";
+    }
+
+
+</script>
+
+<style>
+    .blue {
+        color: blue;
+    }
+
+    .red {
+        color: red;
+    }
+</style>
+
+<h1>{name}</h1>
+<h2>{upperName}</h2>
+<h2>{lowerName}</h2>
+
+<button on:click={chaneName}>Change name</button>
+
+<h1 class={counterClass}>Counter {counter}</h1>
+<button on:click={() => counter++}>Add 1 to counter</button>
